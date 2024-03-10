86819b486260e92f1929136ac91eef9986745250
Viiiiikaaaaa <116728003+Viiiiikaaaaa@users.noreply.github.com>
2024-03-10 21:36:55 +0300

    Author

--- Author.java
+++ Author.java
@@ -11,5 +11,25 @@ public class Author {
 
     public void setName(String name) {
         this.name = name;
+    
+    @Override
+    public String toString() {
+        return "Автор {" +
+                "имя ='" + name + '\'' +
+                '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+    
 }
